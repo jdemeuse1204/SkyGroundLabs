77e6d1f759311b61a5b9632800ff0201453e82f4
jdemeuse1204 <James.demeuse@gmail.com>
2014-11-04 13:59:35 -0600

    11/4/2014
    Changes to reflection class and linq data.  Move linq data ToObject
    extension

--- SkyGroundLabs/SkyGroundLabs.Data.Entity/Extension.cs
+++ SkyGroundLabs/SkyGroundLabs.Data.Entity/Extension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,39 @@ namespace SkyGroundLabs.Data.Entity
 {
 	public static class Extension
 	{
+		/// <summary>
+		/// Converts a SqlDataReader to an object.  The return column names must match the properties names for it to work
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="reader"></param>
+		/// <returns></returns>
+		public static T ToObject<T>(this SqlDataReader reader, params string[] namesToSkip)
+		{
+			T obj = Activator.CreateInstance<T>();
+
+			foreach (var property in obj.GetType().GetProperties())
+			{
+				if (namesToSkip.Contains(property.Name))
+				{
+					continue;
+				}
+
+				property.SetValue(obj, reader[property.Name], null);
+			}
+
+			return obj;
+		}
+
+		/// <summary>
+		/// This is depreciated, please use Entity Frameworks Find Command.
+		/// This was for Linq.Data.Sql because it does not have a find command.
+		/// </summary>
+		/// <typeparam name="TEntity"></typeparam>
+		/// <typeparam name="TPKType"></typeparam>
+		/// <param name="table"></param>
+		/// <param name="ID"></param>
+		/// <param name="pkName"></param>
+		/// <returns></returns>
 		public static TEntity Find<TEntity, TPKType>(this IDbSet<TEntity> table, TPKType ID, string pkName = "ID")
 			where TEntity : DbTableEquatable<IDbTableEquatable<TPKType>>
 			where TPKType : struct
---
+++ SkyGroundLabs/SkyGroundLabs.Reflection/Extension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+	public static class Extension
+	{
+		public static T GetCustomAttribute<T>(this PropertyInfo property) where T : Attribute
+		{
+			var result = property.GetCustomAttributes(typeof(T), true).FirstOrDefault();
+
+			if (result == null)
+			{
+				return default(T);
+			}
+
+			return (T)result;
+		}
+
+		public static List<object> GetCustomAttributes<T>(this PropertyInfo property) where T : Attribute
+		{
+			var result = property.GetCustomAttributes(typeof(T), true).ToList();
+
+			if (result == null)
+			{
+				return null;
+			}
+
+			return result;
+		}
+	}
+}
